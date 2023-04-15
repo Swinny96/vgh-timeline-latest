fa1dd959cf21faaba0c757fe49c15fb7cd796562
Christopher Swinton <chris@theswintons.co.uk>
2023-04-15 23:25:19 +0100

    Added Header Context

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.scss";
-import Interactive from "./components/interactive";
+import Timeline from "./components/timeline";
 
 function App() {
   return (
-    <main className="Content">
-      <Interactive />
-    </main>
+    <div>
+      <header>
+        <h1 className="heading">Welcome to Video Game History</h1>
+        <p className="blerb">
+          Learn about the history of video game home consoles with this
+          interactive timeline, take a look at all the major Consoles that have
+          come out since the birth of console gaming
+        </p>
+        <p className="blerb">
+          See how far the history goes back, what systems were made, who made
+          them, how well they sold and the best selling games on the platforms
+        </p>
+      </header>
+      <main className="Content">
+        <Timeline />
+      </main>
+      <footer></footer>
+    </div>
   );
 }
 
--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,44 +1,65 @@
-import * as React from "react";
-import Timeline from "@mui/lab/Timeline";
-import TimelineItem from "@mui/lab/TimelineItem";
-import TimelineSeparator from "@mui/lab/TimelineSeparator";
-import TimelineConnector from "@mui/lab/TimelineConnector";
-import TimelineContent from "@mui/lab/TimelineContent";
-import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
-import TimelineDot from "@mui/lab/TimelineDot";
-import Typography from "@mui/material/Typography";
-import Data from "./vgh";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
-export default function CustomizedTimeline() {
+import Data from "./history";
+
+export default function Timeline() {
   return (
-    <Timeline position="alternate">
-      {Data.map(({ id, image, align, variant, color, backgrond, year, system, describe, icon  }) => {
-        return (
-          <TimelineItem>
-            <TimelineOppositeContent
-              sx={{ m: "auto 0" }}
-              align={align}
-              variant={variant}
-              color={color}
+    <VerticalTimeline layout="2-columns" animate="true" lineColor="white">
+      {Data.map(
+        ({
+          id,
+          image,
+          abbreviation,
+          gen,
+          year,
+          system,
+          describe,
+          icon,
+          manufacture,
+          units,
+          game1,
+          game2,
+          game3,
+          game4,
+          game5,
+        }) => {
+          return (
+            <VerticalTimelineElement
+              className={"vertical-timeline-element--" + { system }}
+              id={abbreviation}
+              date={year}
+              icon={icon}
             >
-              {year}
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineConnector />
-              <TimelineDot color={backgrond}>
-                {icon}
-              </TimelineDot>
-              <TimelineConnector />
-            </TimelineSeparator>
-            <TimelineContent sx={{ py: "12px", px: 2 }}>
-              <Typography variant="h6" component="span">
-                {system}
-              </Typography>
-              <Typography>{describe}</Typography>
-            </TimelineContent>
-          </TimelineItem>
-        );
-      })}
-    </Timeline>
+              <span className="units">{units} Units Sold</span>
+              <h3 className="vertical-timeline-element-title">{system}</h3>
+              <h4 className="vertical-timeline-element-subtitle">
+                {manufacture}
+              </h4>
+              <img
+                className="vertical-timeline-element-image"
+                alt={system}
+                src={image}
+                loading="lazy"
+              />
+              <p>{describe}</p>
+              <div className="best-selling-games">
+                <h4>Best Selling Games</h4>
+                <ol>
+                  <li>{game1}</li>
+                  <li>{game2}</li>
+                  <li>{game3}</li>
+                  <li>{game4}</li>
+                  <li>{game5}</li>
+                </ol>
+              </div>
+            </VerticalTimelineElement>
+          );
+        }
+      )}
+    </VerticalTimeline>
   );
 }
